92e7c28398515b860f39da823c2fb4c4727c2fff
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-04-22 00:20:18 +0200

    cleanup

--- Syncer.py
+++ Syncer.py
@@ -184,13 +184,3 @@ class Syncer:
     def sync_all_agents(self):
         agentsyncer = AgentSyncer(emInfraImporter=self.eminfra_importer, neo4J_connector=self.connector)
         agentsyncer.sync_agents()
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    connector = Neo4JConnector("bolt://localhost:7687", "neo4jPython", "python")
-    request_handler = RequestHandler(cert_path=r'C:\resources\datamanager_eminfra_prd.awv.vlaanderen.be.crt',
-                                     key_path=r'C:\resources\datamanager_eminfra_prd.awv.vlaanderen.be.key')
-    eminfra_importer = EMInfraImporter(request_handler)
-    syncer = Syncer(connector=connector, request_handler=request_handler, eminfra_importer=eminfra_importer)
-    syncer.start_syncing()
