ca99eabc7562c29649dc7a2b329172b7f309e7ec
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-04-22 11:38:42 +0200

    add JWT / Cert Authentication possbilities

--- RequestHandler.py
+++ RequestHandler.py
@@ -6,14 +6,9 @@ from requests import Response
 
 
 class RequestHandler:
-    def __init__(self, cert_path: str, key_path: str, base_path: str = 'https://services.apps.mow.vlaanderen.be/eminfra/'):
-        if not os.path.isfile(cert_path):
-            raise FileNotFoundError('certificate file not found')
-        if not os.path.isfile(key_path):
-            raise FileNotFoundError('key file not found')
-        self.cert_path = cert_path
-        self.key_path = key_path
-        self.base_path = base_path
+    def __init__(self, requester, base_path: str = 'https://services.apps.mow.vlaanderen.be/eminfra/'):
+        self.requester = requester
+        requester.first_part_url = base_path
 
     def get_jsondict(self, url):
         response = self._perform_get_request(url)
@@ -22,12 +17,9 @@ class RequestHandler:
         return dict_obj
 
     def _perform_get_request(self, url) -> Response:
-        return requests.get(url=f'{self.base_path}{url}',
-                            cert=(self.cert_path, self.key_path),
-                            headers={"accept": "application/vnd.awv.eminfra.v2+json"})
+        return self.requester.get(url=url)
 
-    def perform_post_request(self, url, json_data=None) -> Response:
-        return requests.post(url=f'{self.base_path}{url}',
-                             cert=(self.cert_path, self.key_path),
-                             headers={"accept": "application/vnd.awv.eminfra.v2+json"},
-                             json=json_data)
+    def perform_post_request(self, url, json_data=None, **kwargs) -> Response:
+        if json_data is not None:
+            kwargs['json'] = json_data
+        return self.requester.post(url=url, **kwargs)
--- main.py
+++ main.py
@@ -3,13 +3,18 @@ import logging
 from EMInfraImporter import EMInfraImporter
 from Neo4JConnector import Neo4JConnector
 from RequestHandler import RequestHandler
+from RequesterFactory import RequesterFactory
+from SettingsManager import SettingsManager
 from Syncer import Syncer
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     connector = Neo4JConnector("bolt://localhost:7687", "neo4jPython", "python")
-    request_handler = RequestHandler(cert_path=r'C:\resources\datamanager_eminfra_prd.awv.vlaanderen.be.crt',
-                                     key_path=r'C:\resources\datamanager_eminfra_prd.awv.vlaanderen.be.key')
+    settings_manager = SettingsManager(settings_path='C:\\resources\\settings_neo4jmodelcreator.json')
+
+    requester = RequesterFactory.create_requester(settings=settings_manager.settings, auth_type='JWT', env='prd')
+    request_handler = RequestHandler(requester)
+
     eminfra_importer = EMInfraImporter(request_handler)
     syncer = Syncer(connector=connector, request_handler=request_handler, eminfra_importer=eminfra_importer)
     syncer.start_syncing()
