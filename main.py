f2831965a3552ea18ff8175a42b014e324b392d2
Arthur <devosarthur@gmail.com>
2022-10-20 18:08:23 +0200

    Update

--- main.py
+++ main.py
@@ -10,12 +10,11 @@ from Syncer import Syncer
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     connector = Neo4JConnector(uri="bolt://localhost:7687", user="neo4jPython", password="python")
-    settings_manager = SettingsManager(settings_path='/home/davidlinux/Documents/AWV/resources/settings_neo4jmodelcreator.json')
-    # settings_manager = SettingsManager(settings_path='C:\\resources\\settings_neo4jmodelcreator.json')
+    settings_manager = SettingsManager(settings_path='.\\settings_neo4jmodelcreator.json')
 
     requester = RequesterFactory.create_requester(settings=settings_manager.settings, auth_type='JWT', env='prd')
     request_handler = RequestHandler(requester)
 
     eminfra_importer = EMInfraImporter(request_handler)
     syncer = Syncer(connector=connector, request_handler=request_handler, eminfra_importer=eminfra_importer, settings=settings_manager.settings)
-    syncer.start_syncing()
+    syncer.start_syncing(stop_when_fully_synced=False)
