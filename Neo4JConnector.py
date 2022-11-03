db192aecc9a3869703abcd2aede368b986f7a3c0
Arthur <devosarthur@gmail.com>
2022-11-03 13:08:29 +0100

    Update

--- Neo4JConnector.py
+++ Neo4JConnector.py
@@ -1,3 +1,5 @@
+import time
+import logging
 from neo4j import GraphDatabase, Transaction
 
 
@@ -11,7 +13,8 @@ class Neo4JConnector:
             params = session.run("MATCH (p:Params) RETURN p").single()
             if params is None:
                 self.set_default_constraints_and_indices(session)
-                params = session.run("CREATE (p:Params {page:-1, event_id:'', pagesize:100, freshstart:True, otltype:-1, cursor:''}) RETURN p").single()
+                params = session.run(
+                    "CREATE (p:Params {page:-1, event_id:'', pagesize:100, freshstart:True, otltype:-1, cursor:''}) RETURN p").single()
             return params[0]
 
     def save_props_to_params(self, tx: Transaction, params: dict):
@@ -68,13 +71,17 @@ class Neo4JConnector:
         assert self.driver is not None, "Driver not initialized!"
         session = None
         response = None
+        while True:
+            try:
+                session = self.driver.session(database=self.db)
+                response = list(session.run(query))
+                return response
+            except Exception as e:
+                logging.error("Query failed:", e)
+                logging.error('Are settings and/or connection to the Neo4J database okay?')
+                logging.info('Retrying in 30 seconds.')
+                time.sleep(30)
+            finally:
+                if session is not None:
+                    session.close()
 
-        try:
-            session = self.driver.session(database=self.db)
-            response = list(session.run(query))
-        except Exception as e:
-            print("Query failed:", e)
-        finally:
-            if session is not None:
-                session.close()
-        return response
