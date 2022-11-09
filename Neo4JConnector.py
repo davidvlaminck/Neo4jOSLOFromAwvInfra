7bd7ee3d718c98d48e1b62b4b54bb1e3e5e7edbf
davidvlaminck <davidvlaminck85@gmail.com>
2022-11-09 10:44:55 +0100

    better error handling when using a connector

--- Neo4JConnector.py
+++ Neo4JConnector.py
@@ -1,3 +1,6 @@
+import logging
+import time
+
 from neo4j import GraphDatabase, Transaction
 
 
@@ -11,10 +14,12 @@ class Neo4JConnector:
             params = session.run("MATCH (p:Params) RETURN p").single()
             if params is None:
                 self.set_default_constraints_and_indices(session)
-                params = session.run("CREATE (p:Params {page:-1, event_id:'', pagesize:100, freshstart:True, otltype:-1, cursor:''}) RETURN p").single()
+                params = session.run("CREATE (p:Params {page:-1, event_id:'', pagesize:100, "
+                                     "freshstart:True, otltype:-1, cursor:''}) RETURN p").single()
             return params[0]
 
-    def save_props_to_params(self, tx: Transaction, params: dict):
+    @staticmethod
+    def save_props_to_params(tx: Transaction, params: dict):
         tx.run(f"MATCH (p:Params) SET p += $params", params=params)
 
     @staticmethod
@@ -60,21 +65,25 @@ class Neo4JConnector:
         tx_context.commit()
         tx_context.close()
 
-    def set_default_constraints_and_indices(self, session: Transaction):
+    @staticmethod
+    def set_default_constraints_and_indices(session: Transaction):
         session.run("CREATE CONSTRAINT Asset_uuid IF NOT EXISTS FOR (n:Asset) REQUIRE n.uuid IS UNIQUE")
         session.run("CREATE CONSTRAINT Agent_uuid IF NOT EXISTS FOR (n:Agent) REQUIRE n.uuid IS UNIQUE")
 
     def query(self, query):
         assert self.driver is not None, "Driver not initialized!"
         session = None
-        response = None
 
-        try:
-            session = self.driver.session(database=self.db)
-            response = list(session.run(query))
-        except Exception as e:
-            print("Query failed:", e)
-        finally:
-            if session is not None:
-                session.close()
-        return response
+        while True:
+            try:
+                session = self.driver.session(database=self.db)
+                response = list(session.run(query))
+                return response
+            except Exception as e:
+                logging.error("Query failed:", e)
+                logging.error('Are settings and/or connection to the Neo4J database okay?')
+                logging.info('Retrying in 30 seconds.')
+                time.sleep(30)
+            finally:
+                if session is not None:
+                    session.close()
