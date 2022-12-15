f8f5cf0f0273ea7d08c1eb57bf8e9eec0e828da6
Arthur <devosarthur@gmail.com>
2022-12-15 12:23:02 +0100

    Update

--- main.py
+++ main.py
@@ -6,20 +6,13 @@ from RequestHandler import RequestHandler
 from RequesterFactory import RequesterFactory
 from SettingsManager import SettingsManager
 from Syncer import Syncer
-from decouple import Config, RepositoryEnv
 
 if __name__ == '__main__':
-    # Settings
     logging.basicConfig(level=logging.INFO)
-    config = Config(RepositoryEnv(r"C:\Users\devosar\PycharmProjects\Intelligent Incident Detection (IID)\utils\.env"))
-
-    # Set up connection to Neo4j instance (DBMS)
-    username = config('username_neo4j', default='')  # not just username because this is predefined variable, see .env
-    password = config('password_neo4j', default='')
-    connector = Neo4JConnector(uri="bolt://localhost:11003", user=username, password=password)
-
-    # Syncing settings
+    connector = Neo4JConnector(uri="bolt://localhost:7687", user="neo4jPython", password="python")
     settings_manager = SettingsManager(settings_path='.\\settings_neo4jmodelcreator.json')
+    # settings_manager = SettingsManager(settings_path='C:\\resources\\settings_neo4jmodelcreator.json')
+
     requester = RequesterFactory.create_requester(settings=settings_manager.settings, auth_type='JWT', env='prd')
     request_handler = RequestHandler(requester)
 
